66c4ee58dc4fa9ed551930f644c46402e6f6a381
Patrick M <patrickmacdonald07@gmail.com>
2022-10-22 12:26:56 -0400

    fixed typo in category model

--- models/index.js
+++ models/index.js
@@ -17,15 +17,15 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: "product_id",
   as: "product_tags",
+  foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: "tag_id",
   as: "products",
+  foreignKey: "tag_id",
 });
 
 module.exports = {
